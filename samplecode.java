45151afb701985d2092a13fcf2acee7c3f04d702
rajiharshini2023-max <rajiharshini2023@gmail.com>
2026-06-04 22:10:38 +0530

    code changed in feature branch

--- samplecode.java
+++ samplecode.java
@@ -4,11 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        System.out.println("Welcome, " + name + "!");
-
-        sc.close();
     }
-}
+CODE CHANGED
